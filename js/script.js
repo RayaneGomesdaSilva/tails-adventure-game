73ae71f5a6c36cab5edd47fed71fdefaab7f5a02
rayane <rayanegmoes7041@gmail.com>
2023-06-18 14:34:28 -0300

    v2

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$("#btnDireita").click(
+    function () {
+        $("#area").animate({ left: "1000px", }, { duration: 1200 })
+    }
+)
+$("#btnEsquerda").click(
+    function () {
+        $("#area").animate({ left: "300px" }, { duration: 1200 })
+    }
+)
+$("#btnDescer").click(
+    function () {
+        $("#area").animate({ top: "500px" }, {duration: 1500})
+    }
+)
+$("#btnSubir").click(
+    function () {
+        $("#area").animate({ top: "105px" }, { duration: 1500 })
+    }
+)
